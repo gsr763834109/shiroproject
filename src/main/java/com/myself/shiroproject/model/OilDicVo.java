cf6bdef5895fb74aed7c863a1af9a1e3f5cf051a
guoshirui <763834109@qq.com>
2020-07-06 10:47:12 +0800

    对象

---
+++ src/main/java/com/myself/shiroproject/model/OilDicVo.java
@@ -0,0 +1,23 @@
+package com.myself.shiroproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName OilDicVo
+ * @Description
+ * @Author vici
+ * @Date 2020/7/3 15:06
+ * @Version V2.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OilDicVo {
+    private String olisDisId;
+
+    private String name;
+
+
+}
